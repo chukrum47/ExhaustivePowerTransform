c995bb887d8e790b2d826f3e5745492c14b86123
Trevor M <trevor_masters@hotmail.com>
2016-05-02 01:06:56 -0400

    Added additional comments to ExhaustivePowerTransform function

--- ExhaustivePowerTransform.r
+++ ExhaustivePowerTransform.r
@@ -20,7 +20,7 @@ ExhaustivePowerTransform = function(
   library(car)
   library(lmtest)
   # Set xrange to be a vector of powers to try
-  if (is.factor(x)) {
+  if (is.factor(x)) { # Don't transform if x is a factor
     xrange=c(1)
     xmin=1
   } else if (xstep==0) {
@@ -29,7 +29,7 @@ ExhaustivePowerTransform = function(
     xrange = seq(xmin, xmax, xstep)
   }
   # Set yrange to be a vector of powers to try
-  if (is.factor(y) | length(y) == 1) {
+  if (is.factor(y) | length(y) == 1) { # Don't transform is y is a factor or if y is not specified (ie. is.na(y))
     yrange=c(1)
     ymin=1
   } else if (ystep==0 | type == "norm" | type == "normCVM" | type == "normLillie" | type == "normPearson") {
@@ -104,11 +104,11 @@ ExhaustivePowerTransform = function(
   }
   if (makeHeatmap) {
     # Generate heatmap matrix
-    if (xstep == 0 | length(xrange) == 1) {
+    if (xstep == 0 | length(xrange) == 1) { # If x has only a single value, we have to make two columns because heatmap.2 will not work with a single column
       mat = matrix(rep(res$Score, 2), byrow=F, ncol=2)
       colnames(mat) = c(xmin, xmin) 
       rownames(mat) = yrange
-    } else if (ystep == 0 | length(yrange) == 1) {
+    } else if (ystep == 0 | length(yrange) == 1) { # If y has only a single value, we have to make two rows because heatmap.2 will not work with a single row
       mat = matrix(rep(res$Score, 2), byrow=T, nrow=2)
       colnames(mat) = xrange
       rownames(mat) = c(ymin, ymin)
@@ -119,64 +119,65 @@ ExhaustivePowerTransform = function(
     }
     # Find max score to add lines on the heatmap at those positions
     max.row = which.max(res$Score)
-    maxx = which(xrange==res[max.row,1])
+    # Must convert the coordinates to the heatmap.2 scale
+    maxx = which(xrange==res[max.row,1]) 
     maxy = length(yrange)-which(yrange==res[max.row,2])+1
-    #return (mat)
-    # Must assign the variables as global variables due to a bug in the 'add.expr' parameter of heatmap which fails to check callers's environment and only checks the global environment. Names are chosen to be suitably long/unique so as to minimize chance of overwritign a global variable.
+    # Must assign the variables as global variables due to a bug in the 'add.expr' parameter of heatmap which fails to check callers's environment and only checks the global environment. Names are chosen to be suitably long/unique so as to minimize chance of overwriting a global variable.
     assign(x="ExhaustivePowerTransformXTemp", value=maxx, envir=.GlobalEnv)
     assign(x="ExhaustivePowerTransformYTemp", value=maxy, envir=.GlobalEnv)
     # Only create space for title if one is requested
     if (main=="") {
       heatmap.2(mat, 
-                Rowv=NA, 
-                Colv=NA, 
-                dendrogram="none", 
-                symkey=F, 
-                symbreaks=F, 
-                density.info="none", 
-                keysize=0.5, 
-                key.title=NA, 
-                key.xlab="Score", 
-                key.par=list(mar=c(3.5,0,3,0)), 
-                lmat=rbind(c(5, 4, 2), c(6, 1, 3)), 
-                lhei=c(1.2, 5), lwid=c(1, 10, 1), 
-                margins=c(4,4),
-                col=rev(rainbow(max( length(table(round(mat,roundDigits))), 5), 
+                Rowv=NA, # Don't reorder rows
+                Colv=NA, # Don't reorder columns
+                dendrogram="none", # Don't create dendrograms
+                symkey=F, # Don't draw extra stuff on heatmap
+                symbreaks=F, # Don't draw extra stuff on heatmap
+                density.info="none", # Don't draw density lines on color key
+                keysize=0.5, # Reduce side of color key
+                key.title=NA, # Don't add title to color key
+                key.xlab="Score", # Add x-axis label for color key
+                key.par=list(mar=c(3.5,0,3,0)), # Set margins on color key element
+                lmat=rbind(c(5, 4, 2), c(6, 1, 3)), # Create extra buffering to left of heatmap/color key to make it appear centered
+                lhei=c(1.2, 5), # Set the relative heights of the color key and heatmap
+                lwid=c(1, 10, 1), # Set the width of the heatmap/color key in respect to the left and right edges
+                margins=c(4,4), # Set the margins of the graph
+                col=rev(rainbow(max( length(table(round(mat,roundDigits))), 5), # Set the number of different colors 
                                 start = 0/6, 
-                                end = 5/6)), 
-                scale="none", 
-                revC=F, 
-                xlab = xlab, 
-                ylab = ylab,  
+                                end = 5/6)), # Set the color palette to be rainbow
+                scale="none", # Don't rescale plot
+                revC=F, # Don't reverse columns' order
+                xlab = xlab, # Set x-axis label
+                ylab = ylab,  # Set y-axis label
                 add.expr=abline(v=ExhaustivePowerTransformXTemp, 
-                                h=ExhaustivePowerTransformYTemp), 
-                trace="none")
+                                h=ExhaustivePowerTransformYTemp), # Add black lines to mark highest location
+                trace="none") # Don't draw extra stuff on heatmap
     } else {
       heatmap.2(mat, 
-                Rowv=NA, 
-                Colv=NA, 
-                dendrogram="none", 
-                symkey=F, 
-                symbreaks=F, 
-                density.info="none", 
-                keysize=0.5, 
-                key.title=NA, 
-                key.xlab="Score", 
-                key.par=list(mar=c(3.5,0,3,0)), 
-                lmat=rbind(c(7,8,9),c(5, 4, 2), c(6, 1, 3)), 
-                lhei=c(0.2,1.2, 5), 
-                lwid=c(1, 10, 1), 
-                margins=c(4,4), 
-                col=rev(rainbow(max( length(table(round(mat,roundDigits))), 5), 
+                Rowv=NA, # Don't reorder rows
+                Colv=NA, # Don't reorder columns
+                dendrogram="none", # Don't create dendrograms
+                symkey=F, # Don't draw extra stuff on heatmap 
+                symbreaks=F, # Don't draw extra stuff on heatmap 
+                density.info="none", # Don't draw density lines on color key
+                keysize=0.5, # Reduce side of color key
+                key.title=NA, # Don't add title to color key
+                key.xlab="Score", # Add x-axis label for color key
+                key.par=list(mar=c(3.5,0,3,0)), # Set margins on color key element
+                lmat=rbind(c(7,8,9),c(5, 4, 2), c(6, 1, 3)), # Create extra buffering to left of heatmap/color key to make it appear centered and extra room above for title
+                lhei=c(0.2,1.2, 5), # Set the relative heights of the title, color key, and heatmap
+                lwid=c(1, 10, 1), # Set the width of the heatmap/color key in respect to the left and right edges
+                margins=c(4,4), # Set the margins of the graph
+                col=rev(rainbow(max( length(table(round(mat,roundDigits))), 5), # Set the number of different colors  
                                 start = 0/6, 
-                                end = 5/6)), 
-                scale="none", 
-                revC=F, 
-                xlab = xlab, 
-                ylab = ylab,  
+                                end = 5/6)), # Set the color palette to be rainbow
+                scale="none", # Don't rescale plot
+                revC=F, # Don't reverse columns' order
+                xlab = xlab, # Set x-axis label
+                ylab = ylab, # Set y-axis label
                 add.expr=abline(v=ExhaustivePowerTransformXTemp, 
-                                h=ExhaustivePowerTransformYTemp), 
-                trace="none")
+                                h=ExhaustivePowerTransformYTemp), # Add black lines to mark highest location
+                trace="none")# Don't draw extra stuff on heatmap
       # Must separately create title due to peculiarities about how the heatmap is laid out
       title(main)
     }
